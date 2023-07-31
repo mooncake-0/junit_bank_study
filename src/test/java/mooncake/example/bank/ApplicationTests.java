311fb39398b3a28cc01690f95d54217f959c35cb
wrock.kang <a@moiming.net>
2023-07-31 20:29:14 +0900

    init

---
+++ src/test/java/mooncake/example/bank/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mooncake.example.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
